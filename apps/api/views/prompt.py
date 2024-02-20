01232b20073e2a881a1e849b5e1f1e64b68b19d3
jesielcarlos <jesielcarlos17@gmail.com>
2024-02-20 01:03:11 -0300

    Instalando cors headers
    modificando função principal do chat
    
    Adicionando url nos allowed origins

--- apps/api/views/prompt.py
+++ apps/api/views/prompt.py
@@ -8,6 +8,7 @@ from django.views.decorators.cache import cache_page
 from django.views.decorators.vary import vary_on_headers
 from django.utils.decorators import method_decorator
 import decouple
+import json
 
 
 API_TOKEN = decouple.config("API_TOKEN", default=None)
@@ -20,7 +21,11 @@ class ChatAPIViewSet(ViewSet):
     # @method_decorator(vary_on_headers("Authorization",))
     
     def get_model_response(self, input_text):
-        response = requests.post(MODEL_URL, headers=headers, json={"inputs": input_text})
+        response = requests.post(
+            MODEL_URL,
+            headers=headers,
+            json={"inputs": input_text}
+        )
         return response.json()
     
     @action(
@@ -29,35 +34,48 @@ class ChatAPIViewSet(ViewSet):
         url_path="chat",
     )
     def query(self, request):
-        chat = []
-        i = 1
-        while True:
-            user_input = request.data.get('prompt')
-            print("sadasdsad")
-            print(user_input)
+        
+        with open("chat_messages.json", 'r') as f:
+            chat = json.loads(f.read())
+            f.close()
+        i = 2
 
-            if user_input:
-                if i == 1:
-                    context = 'A partir de agora você é um chat especializado para customer success, portanto, responda a pergunta a seguir: '
-                    chat.append(context + user_input)
+        if not chat:
+            chat = []
+            i = 1
 
-                    translated_input = GoogleTranslator(source='auto', target='en').translate('\n'.join(chat))
-                    outputs = self.get_model_response(translated_input)
-                    output = outputs[0]['generated_text'].split('\n\n')[1]
-                    translated_output = GoogleTranslator(source='auto', target='pt').translate(output).replace('Answer: ', '')
-                    i = 2
-                else:
-                    chat.append(user_input)
-                    translated_input = GoogleTranslator(source='auto', target='en').translate('Responda apenas a última sentença desta conversa baseando-se no contexto completo' + ('\n'.join(chat)))
-                    outputs = self.get_model_response(translated_input)
-                    output = outputs[0]['generated_text'].split('\n')[len(chat)]
-                    translated_output = GoogleTranslator(source='auto', target='pt').translate(output).replace('Answer: ', '')
+        user_input = request.data.get('prompt')
+        if not user_input:
+            return JsonResponse(
+                {"error": "Prompt is required"},
+                status=HTTP_400_BAD_REQUEST
+            )
 
-                chat.append(translated_output)
+        if user_input:
+            if i == 1:
+                context = 'A partir de agora você é um chat especializado para customer success, portanto, responda a pergunta a seguir: '
+                chat.append(context + user_input)
 
-                if translated_output == 'Encerrar conversa':
-                    break
-
-                return JsonResponse({'resposta': translated_output})
+                translated_input = GoogleTranslator(source='auto', target='en').translate('\n'.join(chat))
+                outputs = self.get_model_response(translated_input)
+                output = outputs[0]['generated_text'].split('\n\n')[1]
+                translated_output = GoogleTranslator(source='auto', target='pt').translate(output).replace('Answer: ', '')
+                i = 2
             else:
-                return JsonResponse({'erro': 'Entrada de usuário não fornecida'}, status=400)
+                chat.append(user_input)
+                translated_input = GoogleTranslator(source='auto', target='en').translate('Responda apenas a última sentença desta conversa baseando-se no contexto completo' + ('\n'.join(chat)))
+                outputs = self.get_model_response(translated_input)
+                output = outputs[0]['generated_text'].split('\n')[len(chat)]
+                translated_output = GoogleTranslator(source='auto', target='pt').translate(output).replace('Answer: ', '')
+
+            chat.append(translated_output)
+
+            with open("chat_messages.json", 'w') as f:
+                f.write(json.dumps(chat, indent=4, ensure_ascii=False))
+                f.close()
+            return JsonResponse({'message': translated_output})
+        else:
+            return JsonResponse(
+                {'erro': 'Entrada de usuário não fornecida'},
+                status=400
+            )
