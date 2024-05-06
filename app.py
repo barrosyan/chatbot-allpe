39e4639e56afea9b07e3105fd4d982cf5323e48e
yan <yanbarrosyan@gmail.com>
2024-05-06 09:16:20 -0300

    check the result of similarity and if its too slow, send a different message

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import numpy as np
 
 app = Flask(__name__)
 
-with open('dataset.json', 'r', encoding='utf-8') as file:
+with open('/home/barrosyan/mysite/dataset.json', 'r', encoding='utf-8') as file:
     data = json.load(file)
 
 user_sentences = []
@@ -30,13 +30,25 @@ def get_most_similar_tag(sentence):
     most_similar_tag_index = np.argmax(similarities)
     return most_similar_tag_index
 
-@app.route('/get_response', methods=['POST'])
+def treatment_of_string(word):
+    word = word.lower()
+    word=word.replace('á','a').replace('é','e').replace('í','i').replace('ó','o').replace('ú','u')
+    word=word.replace('â','a').replace('ê','e').replace('î','i').replace('ô','o').replace('û','u')
+    word=word.replace('ã','a').replace('õ','o')
+    word=word.replace('ç','c').replace('@','').replace("'",'').replace('"','').replace('#','').replace('$','').replace('%','').replace('&','').replace('*','')
+    return word
+
+@app.route('/', methods=['GET', 'POST'])
 def get_response():
     user_input = request.json['user_input']
-    most_similar_tag_index = get_most_similar_tag(user_input)
-    answers_for_tag = data[most_similar_tag_index]['answers']
-    chosen_answer = np.random.choice(answers_for_tag)
+    user_input = treatment_of_string(user_input)
+    most_similar_tag_index, result = get_most_similar_tag(user_input)
+    if result < 0.1:
+        chosen_answer = 'Desculpa, ainda não aprendi sobre este tema. Irei repassar para um funcionário mais qualificado. Aguarde um instante, por favor :)'
+    else:
+        answers_for_tag = data[most_similar_tag_index]['answers']
+        chosen_answer = np.random.choice(answers_for_tag)
     return jsonify({'response': chosen_answer})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
